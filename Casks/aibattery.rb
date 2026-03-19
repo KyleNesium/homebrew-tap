54e841c05b07fda846c6abc56704a73322cf1830
Kyle Steenkamp <KyleNesium@users.noreply.github.com>
2026-03-20 00:53:26 +0200

    Update aibattery cask to v1.9.4

--- Casks/aibattery.rb
+++ Casks/aibattery.rb
@@ -1,6 +1,6 @@
 cask "aibattery" do
-  version "1.9.3"
-  sha256 "cab7f5176d34bb23a3341af33e17bad9a3c760230c7b7ba326b88f5cb28bf1ba"
+  version "1.9.4"
+  sha256 "f1f45e7c92e2f3a674d202c7b72af4ea0fe8967a7dc59ce5a14bd2a33de064dd"
 
   url "https://github.com/KyleNesium/AIBattery/releases/download/v#{version}/AIBattery.zip"
   name "AI Battery"
